f10e197e560b52c9b5c56a5c75bad0765554b3a4
John Wass <wassj@ctc.com>
2022-01-04 17:25:43 -0500

    flush notify marks on mount error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::error::Error;
+
 use clap::Parser;
-use fanotify::high_level::*;
 use fanotify::high_level::FanEvent::OpenExecPerm;
 use fanotify::high_level::FanotifyResponse::Allow;
-use fanotify::low_level::{FAN_OPEN_EXEC_PERM, FAN_OPEN_PERM, fanotify_read};
+use fanotify::high_level::*;
 use fanotify::low_level;
+use fanotify::low_level::{fanotify_read, FAN_OPEN_EXEC_PERM, FAN_OPEN_PERM};
 use nix::poll::{poll, PollFd, PollFlags};
 use sysinfo::{ProcessExt, ProcessRefreshKind, RefreshKind, System, SystemExt};
 
@@ -13,16 +15,24 @@ struct Args {
     path: String,
 }
 
-fn main() {
+const MOUNT_MASK: u64 = FAN_OPEN_EXEC_PERM | FAN_OPEN_PERM;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let mut system = System::new_with_specifics(RefreshKind::new().with_processes(ProcessRefreshKind::everything()));
+    let mut system = System::new_with_specifics(
+        RefreshKind::new().with_processes(ProcessRefreshKind::everything()),
+    );
 
     let fan = Fanotify::new_with_nonblocking(FanotifyMode::CONTENT);
-    fan.add_mountpoint(FAN_OPEN_EXEC_PERM | FAN_OPEN_PERM, &args.path).unwrap();
+    if fan.add_mountpoint(MOUNT_MASK, &args.path).is_err() {
+        fan.flush_path(0, &args.path)?;
+        fan.add_mountpoint(MOUNT_MASK, &args.path)?;
+    };
+    eprintln!("mounted {}", args.path);
 
     more_magic::initialize();
-    println!("polling");
+    eprintln!("polling");
 
     let mut fds = [PollFd::new(fan.as_raw_fd(), PollFlags::POLLIN)];
     loop {
@@ -33,7 +43,11 @@ fn main() {
                 let event = ext.event;
                 let p = system.process(event.pid as i32).unwrap();
 
-                let perm = if event.events.contains(&OpenExecPerm) { "execute" } else { "open" };
+                let perm = if event.events.contains(&OpenExecPerm) {
+                    "execute"
+                } else {
+                    "open"
+                };
                 let uid = p.uid;
                 let gid = p.gid;
                 let exe = p.exe().to_str().unwrap();
@@ -41,7 +55,10 @@ fn main() {
                 let mime = ext.ftype;
 
                 // rule=9 dec=allow perm=execute uid=1003 gid=100 pid=5555 exe=/usr/bin/bash : path=/usr/bin/vi ftype=application/x-executable
-                println!("rule=? dec=? perm={} uid={} gid={} pid={} exe={} : path={} ftype={}", perm, uid, gid, event.pid, exe, path, mime);
+                println!(
+                    "rule=1 dec=allow perm={} uid={} gid={} pid={} exe={} : path={} ftype={}",
+                    perm, uid, gid, event.pid, exe, path, mime
+                );
 
                 fan.send_response(event.fd, Allow);
             }
@@ -52,6 +69,8 @@ fn main() {
     }
 
     more_magic::destroy();
+
+    Ok(())
 }
 
 struct EventExt {
@@ -64,19 +83,15 @@ trait ExtEvents {
 }
 
 impl ExtEvents for fanotify::high_level::Fanotify {
-    fn read_events_ext(&self) -> Vec<EventExt>  {
+    fn read_events_ext(&self) -> Vec<EventExt> {
         let mut result = Vec::new();
         let events = fanotify_read(self.as_raw_fd());
         for metadata in events {
             let event: Event = metadata.into();
             let ftype = more_magic::get_ftype(event.fd, &event.path);
-            result.push(EventExt {
-                event,
-                ftype,
-            });
+            result.push(EventExt { event, ftype });
             low_level::close_fd(metadata.fd);
         }
         result
     }
 }
-
